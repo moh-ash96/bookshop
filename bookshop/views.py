ea2b1000f2d9b7c5db417125786cc79a4a987237
moh-ash96 <moh.ash96@gmail.com>
2021-06-17 00:00:25 +0300

    Done

--- bookshop/views.py
+++ bookshop/views.py
@@ -11,24 +11,24 @@ from django.urls import reverse_lazy
 
 # Create your views here.
 class Book_list_view(ListView):
-    template_name = 'book_list.html'
+    template_name = 'bookshop/book_list.html'
     model = Book
 
 class Book_detail_view(DetailView):
-    template_name = 'book_detail.html'
+    template_name = 'bookshop/book_detail.html'
     model = Book
 
 class Book_create_view(CreateView):
-    template_name = 'book_create.html'
+    template_name = 'bookshop/book_create.html'
     model = Book
     fields = ["title", "author", "publish_date", "isbn", "seller"]
 
 class Book_update_view(UpdateView):
-    template_name = 'book_update.html'
+    template_name = 'bookshop/book_update.html'
     model = Book
     fields = ["title", "author", "publish_date", "isbn", "seller"]
 
 class Book_delete_view(DeleteView):
-    template_name = 'book_delete.html'
+    template_name = 'bookshop/book_delete.html'
     model = Book
     success_url = reverse_lazy('book_list')
